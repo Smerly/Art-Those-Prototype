eb6c39030a6fb5578f0649f45fa3e68a8bba26fb
Ernest Cheung <ErnestCheung02@gmail.com>
2020-10-17 18:19:13 -0700

    Update ask.js

--- ask.js
+++ ask.js
@@ -1,2 +1,18 @@
-imageInput = document.querySelector("image-input");
-textBox = document.querySelector("text-box");
+const imageInput = document.querySelector("image-input");
+const textBox = document.querySelector("text-box");
+
+const imageInputOut = document.querySelector("display-ask-image");
+const textBoxOut = document.querySelector("display-ask-text");
+
+const postButton = document.querySelector("post-button");
+
+imageInput.addEventListener("input", putIn);
+textBox.addEventListener("input", putIn);
+postButton.addEventListener("click", postThose);
+
+if (postButton.getElementById("button").clicked == true) {
+  textBox = textBoxOut;
+  imageInput = imageInputOut;
+}
+
+function putIn(e) {}
