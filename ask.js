da21917676bc3d6cbff9cad60c519f6525ff41ac
Ernest Cheung <ErnestCheung02@gmail.com>
2020-10-18 14:08:15 -0700

    Update ask.js

--- ask.js
+++ ask.js
@@ -1,18 +1,38 @@
-const imageInput = document.querySelector("image-input");
-const textBox = document.querySelector("text-box");
+// const imageInput = document.querySelector("#image-input");
+// const textBox = document.querySelector(".text-box");
 
-const imageInputOut = document.querySelector("display-ask-image");
-const textBoxOut = document.querySelector("display-ask-text");
+// const imageInputOut = document.querySelector("#display-ask-image");
+// const textBoxOut = document.querySelector("#text-box-out");
 
-const postButton = document.querySelector("post-button");
+// const postButton = document.querySelector("#post-button");
 
-imageInput.addEventListener("input", putIn);
-textBox.addEventListener("input", putIn);
-postButton.addEventListener("click", postThose);
+// imageInput.addEventListener("input", putThose);
+// textBox.addEventListener("input", putThose);
+// postButton.addEventListener("click", postThose);
 
-if (postButton.getElementById("button").clicked == true) {
-  textBox = textBoxOut;
-  imageInput = imageInputOut;
+// function putThose(e) {
+//   if (postButton.getElementById("button").clicked == true) {
+//     const image = imageInput;
+//     const text = textBox.nodeValue;
+
+//     imageInputOut.innerHTML = image;
+//     textBoxOut.innerHTML = text;
+//   }
+// }
+// function postThose(e) {}
+
+// ----------------------------------------------------------------------------------
+
+function save() {
+  var new_data = document.getElementById("text-box").value;
+
+  if (localStorage.getItem("dataText") == null) {
+    localStorage.setItem("dataText", "[]");
+  }
+  var old_data = JSON.parse(localStorage.getItem("dataText"));
+  old_data.push(new_data);
+
+  localStorage.setItem("dataText", JSON.stringify(old_data));
 }
 
-function putIn(e) {}
+function view() {}
